e5e9bd603efcbd604bd25814c0f054844ee2e1eb
hawa130 <handanwanga@hotmail.com>
2024-11-07 01:07:21 +0800

    🔧 update meta

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -4,6 +4,12 @@ import { TanStackRouterRspack } from '@tanstack/router-plugin/rspack'
 
 export default defineConfig({
   plugins: [pluginReact()],
+  html: {
+    title: '表单编辑器 Form Crafter',
+    meta: {
+      description: '一款简单的表单编辑器',
+    },
+  },
   tools: {
     rspack: {
       plugins: [TanStackRouterRspack()],
